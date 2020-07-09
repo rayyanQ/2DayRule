ab4daca9d59a87862da6e1e39b2dcc9657958418
Rayyan <5747484+rayyanQ@users.noreply.github.com>
2020-07-09 02:07:49 -0400

    changed color

--- src/components/header.js
+++ src/components/header.js
@@ -5,14 +5,14 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#FFFBF8`,
       marginBottom: `1.45rem`,
     }}
   >
     <div
       style={{
         margin: `0 auto`,
-        maxWidth: 960,
+        maxWidth: 1200,
         padding: `1.45rem 1.0875rem`,
       }}
     >
@@ -20,7 +20,7 @@ const Header = ({ siteTitle }) => (
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `#403E3D`,
             textDecoration: `none`,
           }}
         >
