84f4500f1d31dc4f6e81bfd9cfb4ad3fa0fb28b6
Rayyan <5747484+rayyanQ@users.noreply.github.com>
2020-07-19 01:58:48 -0400

    reordered the layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,16 +14,16 @@ const IndexPage = () => (
       <h1 id="hero" className="d-flex justify-content-center align-items-center" style={{ fontWeight: `bolder`, height: `350px` }}>
         Never skip 2 days of a habit in a row
       </h1>
-      <p style={{ lineHeight: `2`, textAlign: `justify` }}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis tellus id interdum velit laoreet id donec ultrices. Ut pharetra sit amet aliquam. Quam viverra orci sagittis eu volutpat odio facilisis mauris. Morbi tincidunt ornare massa eget egestas purus viverra. Quam quisque id diam vel. Amet mauris commodo quis imperdiet massa tincidunt. Condimentum mattis pellentesque id nibh. Ut consequat semper viverra nam. Consectetur a erat nam at lectus urna duis. Risus viverra adipiscing at in tellus integer feugiat. Turpis massa sed elementum tempus egestas sed sed risus. Ipsum a arcu cursus vitae congue mauris rhoncus. Habitasse platea dictumst vestibulum rhoncus est pellentesque elit.
-      </p>
       <iframe className="my-5" width="100%" height="500" src="https://www.youtube.com/embed/bfLHTLQZ5nc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <span>
+      <p style={{ lineHeight: `2`, textAlign: `justify` }}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis tellus id interdum velit laoreet id donec ultrices. Ut pharetra sit amet aliquam. Quam viverra orci sagittis eu volutpat odio facilisis mauris. Morbi tincidunt ornare massa eget egestas purus viverra. Quam quisque id diam vel. Amet mauris commodo quis imperdiet massa tincidunt. Condimentum mattis pellentesque id nibh. Ut consequat semper viverra nam. Consectetur a erat nam at lectus urna duis.
+      </p>
+      <div className="my-5">
         <p className="h4">Why it works</p>
         <p style={{ lineHeight: `2`, textAlign: `justify` }}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis tellus id interdum velit laoreet id donec ultrices. Ut pharetra sit amet aliquam. Quam viverra orci sagittis eu volutpat odio facilisis mauris. Morbi tincidunt ornare massa eget egestas purus viverra. Quam quisque id diam vel. Amet mauris commodo quis imperdiet massa tincidunt. Condimentum mattis pellentesque id nibh. Ut consequat semper viverra nam. Consectetur a erat nam at lectus urna duis. Risus viverra adipiscing at in tellus integer feugiat.
         </p>
-      </span>
+      </div>
       <div className="my-5">
         <p className="h4">Track your habit</p>
         <p style={{ lineHeight: `2`, textAlign: `justify` }}>
