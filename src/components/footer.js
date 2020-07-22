c95913b5dc6d8ef1fb75f621c387b2256ad5c7c0
Rayyan <5747484+rayyanQ@users.noreply.github.com>
2020-07-22 15:53:52 -0400

    updated copy

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,10 +3,12 @@ import React from "react"
 
 const Footer = ({ siteTitle }) => (
   <footer style={{ background: `white` }}>
-    <div className="container d-flex align-items-start justify-content-end py-3">
-        
+    <div style={{ height: `200px` }} className="container d-flex flex-column align-items-end justify-content-around py-3">
+      <a href="https://twitter.com/RayyanQuraishi"><img src="https://img.icons8.com/color/48/000000/twitter-circled.png"/></a>
+      <a href="https://github.com/rayyanQ"><img src="https://img.icons8.com/fluent/48/000000/github.png"/></a>
+      <a href="https://www.instagram.com/rayyanquraishi/"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
     </div>
-    <div style={{ height: `350px` }} className="container d-flex align-items-end justify-content-between py-3">
+    <div style={{ height: `150px` }} className="container d-flex align-items-end justify-content-between py-3">
         <Link to="/">
             Made by Rayyan Ahmed Quraishi
         </Link>
@@ -15,6 +17,9 @@ const Footer = ({ siteTitle }) => (
         <Link className="pl-4">About</Link>
       </span>
     </div>
+    <a href="https://icons8.com/icon/114450/twitter-circled">Twitter Circled icon by Icons8</a>
+    <a href="https://icons8.com/icon/AZOZNnY73haj/github">GitHub icon by Icons8</a>
+    <a href="https://icons8.com/icon/Xy10Jcu1L2Su/instagram">Instagram icon by Icons8</a>
   </footer>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -16,18 +16,18 @@ const IndexPage = () => (
       </h1>
       <iframe className="my-5" width="100%" height="500" src="https://www.youtube.com/embed/bfLHTLQZ5nc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       <p style={{ lineHeight: `2`, textAlign: `justify` }}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis tellus id interdum velit laoreet id donec ultrices. Ut pharetra sit amet aliquam. Quam viverra orci sagittis eu volutpat odio facilisis mauris. Morbi tincidunt ornare massa eget egestas purus viverra. Quam quisque id diam vel. Amet mauris commodo quis imperdiet massa tincidunt. Condimentum mattis pellentesque id nibh. Ut consequat semper viverra nam. Consectetur a erat nam at lectus urna duis.
+        The 2 Day Rule was introduced by Matt D’Avella as a simple way to build habits. He talks about how rules are not necessarily bad and are essential for having some structure in life. One such rule that can help you build habits is the 2 Day Rule. It's simple: Never skip 2 days of a habit in a row. Take for instance, exercise. Set a minimum for yourself, say 10 pushups, and every day no matter what you must meet your minimum goal. You might have a whole exercise routine, but on days that you don't have time or are just not in the mood, all you have to do is just hit the minimum. This way you stay consistent and don't derail after the initial excitement.
       </p>
       <div className="my-5">
         <p className="h4">Why it works</p>
         <p style={{ lineHeight: `2`, textAlign: `justify` }}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis tellus id interdum velit laoreet id donec ultrices. Ut pharetra sit amet aliquam. Quam viverra orci sagittis eu volutpat odio facilisis mauris. Morbi tincidunt ornare massa eget egestas purus viverra. Quam quisque id diam vel. Amet mauris commodo quis imperdiet massa tincidunt. Condimentum mattis pellentesque id nibh. Ut consequat semper viverra nam. Consectetur a erat nam at lectus urna duis. Risus viverra adipiscing at in tellus integer feugiat.
+          The 2 Day Rule works for two reasons: it allows you to do an "ok" job, and it forces you to stay consistent while giving room to screw up. People are usually not able to stick to new habits because they are not consistent enough, and people are not consistent enough because they think that they have to do a great job every single time. For instance, if you want to get into the habit of writing, you might think you have to produce a few pages everyday. By holding ourselves to this impossible standard we are making it difficult to start. But if you give yourself an easy minimum, say 500 words per day, we make it easy to stay consistent and do the work even on bad days.
         </p>
       </div>
       <div className="my-5">
         <p className="h4">Track your habit</p>
         <p style={{ lineHeight: `2`, textAlign: `justify` }}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Convallis tellus id interdum velit laoreet id donec ultrices. Ut pharetra sit amet aliquam. Quam viverra orci sagittis eu volutpat odio facilisis mauris. Morbi tincidunt ornare massa eget egestas purus viverra. Quam quisque id diam vel. Amet mauris commodo quis imperdiet massa tincidunt. Condimentum mattis pellentesque id nibh.
+          Just mark your progress on a calendar. It is very important to visually see the progress that you make, it can be very encouraging and motivating. I would recommend using a physical paper calendar, though you could use an online app to do the same. We are currently building a free and opensource tool specifically for the 2 Day Rule, click here if you would like to contribute.
         </p>
       </div>
     </div>
